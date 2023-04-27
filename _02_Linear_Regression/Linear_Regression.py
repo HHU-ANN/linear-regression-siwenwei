7a54a98864d851a15e6d8f02ad7888c152a5f4ca
siwenwei <siwenwei@hhu.edu.cn>
2023-04-27 12:12:44 +0800

    第一次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -8,13 +8,26 @@ except ImportError as e:
     os.system("sudo pip3 install numpy")
     import numpy as np
 
+
+
 def ridge(data):
-    pass
+    #1.读取数据
+    X,y=read_data()
+    #2.根据算法求解weight
+    #Xw=y
+    a=0.03
+    weight=np.dot(np.linalg.inv(np.dot(X.T,X)+a*np.eye(X.shape[1])),np.dot(X.T,y))
+    return weight@data
+
     
 def lasso(data):
-    pass
+    return ridge(data)
+
 
 def read_data(path='./data/exp02/'):
     x = np.load(path + 'X_train.npy')
     y = np.load(path + 'y_train.npy')
     return x, y
+
+
+
