9caa757248ac4b46ac81dbc56fe75888084ee842
siwenwei <siwenwei@hhu.edu.cn>
2023-05-05 22:34:41 +0800

    第一次提交

--- _02_Linear_Regression/Linear_Regression.py
+++ _02_Linear_Regression/Linear_Regression.py
@@ -1,6 +1,7 @@
 # 最终在main函数中传入一个维度为6的numpy数组，输出预测值
-
+from matplotlib import pyplot as plt
 import os
+import math
 import math
 try:
     import numpy as np
@@ -15,9 +16,18 @@ def ridge(data):
     X,y=read_data()
     #2.根据算法求解weight
     #Xw=y
-    a=math.exp(-4.5)
-    #0.018189888888888888882611
+    a=0.5
+    add11=X[:,4]*X[:,5]
+    add12=X[:,2]*X[:,3]
+    add13=X[:,0]*X[:,1]
+    X=np.column_stack((X,add11,add12,add13))
+    X=np.concatenate((np.ones((404, 1)), X), axis=1)
     weight=np.dot(np.linalg.inv(np.dot(X.T,X)+a*np.eye(X.shape[1])),np.dot(X.T,y))
+    add21=data[:,4]*data[:,5]
+    add22=data[:,2]*data[:,3]
+    add23=data[:,0]*data[:,1]
+    data=np.column_stack((data,add21,add22,add23))
+    data=np.concatenate((np.ones((10,1)),data),axis=1)
     return data@weight
 
 
@@ -27,42 +37,41 @@ def ridge(data):
 
 def lasso(data):
     X,y=read_data()
-    #print(X.shape[1])
-    a=0.6
+
+    add13 = X[:, 0] * X[:, 3]/10
+    add14 = X[:,1] * X[:,2]/10
+    add15 = X[:,1] * X[:,3]/10
+    add16 = X[:,1] * X[:,4]/10
+    add17 = X[:,2] * X[:,3]/10
+    add18 = X[:,2] * X[:,4]/10
+    add19 = X[:,3] * X[:,5]/10
+    X=np.column_stack((X,add13,add14,add15,add16,add17,add18,add19))
+
+    a=0.5
     w=np.zeros(X.shape[1])
-    #print(w)
-    b=0
-    learning_rate=0.0000003
+
+
+    learning_rate=1e-8
     num_train = X.shape[0]
-    num_feature = X.shape[1]
 
-    y_hat = np.dot(X, w) + b
-    loss = np.sum((y_hat - y) ** 2) / num_train + np.sum(a * abs(w))
-    dw = np.dot(X.T, (y_hat - y)) / num_train + a * np.sign(w)
-    db = np.sum((y_hat - y)) / num_train
-    loss_list=[]
-    epochs=10
-    params = {
-        'w': w,
-        'b': b
-    }
+
+    add23 = data[:,0] * data[:,3]/10
+    add24 = data[:, 1] * data[:, 2]/10
+    add25 = data[:, 1] * data[:, 3]/10
+    add26 = data[:, 1] * data[:, 4]/10
+    add27 = data[:, 2] * data[:, 3]/10
+    add28 = data[:, 2] * data[:, 4]/10
+    add29 = data[:, 3] * data[:, 5]/10
+
+    data=np.column_stack((data,add23,add24,add25,add26,add27,add28,add29))
+
+
+    epochs=500000
+
     for i in range(1,epochs):
-        y_hat = np.dot(X, w) + b
-        loss = np.sum((y_hat - y) ** 2) / num_train + np.sum(a * abs(w))
-        dw = np.dot(X.T, (y_hat - y)) / num_train + a * np.sign(w)
-        db = np.sum((y_hat - y)) / num_train
-        w+=-learning_rate*dw
-        b+=-learning_rate*db
-        loss_list.append(loss)
-        params={
-                'w':w,
-                'b':b
-            }
-
-    w=params['w']
-    b=params['b']
-    y_pred = np.dot(data, w)+b
-    #print(y_pred.shape)
+        grad = (np.dot(X.T, np.dot(X, w) - y) / num_train) + a * np.sign(w)
+        w = w - learning_rate * grad
+    y_pred = np.dot(data, w)
     return y_pred
 
 
@@ -73,24 +82,6 @@ def read_data(path='./data/exp02/'):
     y = np.load(path + 'y_train.npy')
     return x, y
 
-features = np.array([
-    [2.0133330e+03, 1.6400000e+01, 2.8932480e+02, 5.0000000e+00, 2.4982030e+01, 1.2154348e+02],
-    [2.0126670e+03, 2.3000000e+01, 1.3099450e+02, 6.0000000e+00, 2.4956630e+01, 1.2153765e+02],
-    [2.0131670e+03, 1.9000000e+00, 3.7213860e+02, 7.0000000e+00, 2.4972930e+01, 1.2154026e+02],
-    [2.0130000e+03, 5.2000000e+00, 2.4089930e+03, 0.0000000e+00, 2.4955050e+01, 1.2155964e+02],
-    [2.0134170e+03, 1.8500000e+01, 2.1757440e+03, 3.0000000e+00, 2.4963300e+01, 1.2151243e+02],
-    [2.0130000e+03, 1.3700000e+01, 4.0820150e+03, 0.0000000e+00, 2.4941550e+01, 1.2150381e+02],
-    [2.0126670e+03, 5.6000000e+00, 9.0456060e+01, 9.0000000e+00, 2.4974330e+01, 1.2154310e+02],
-    [2.0132500e+03, 1.8800000e+01, 3.9096960e+02, 7.0000000e+00, 2.4979230e+01, 1.2153986e+02],
-    [2.0130000e+03, 8.1000000e+00, 1.0481010e+02, 5.0000000e+00, 2.4966740e+01, 1.2154067e+02],
-    [2.0135000e+03, 6.5000000e+00, 9.0456060e+01, 9.0000000e+00, 2.4974330e+01, 1.2154310e+02]
-    ])
-print(ridge(features))
-labels = np.array([41.2, 37.2, 40.5, 22.3, 28.1, 15.4, 50. , 40.6, 52.5, 63.9])
-print(labels)
-print(lasso(features))
-print(ridge(features)-labels)
-print(lasso(features)-labels)
 
 
 
